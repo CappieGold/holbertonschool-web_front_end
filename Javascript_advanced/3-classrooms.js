ba229a5abd6b5cf8ee94ad9081354410b33a4560
CappieGold <144028326+CappieGold@users.noreply.github.com>
2025-04-04 11:42:49 +0000

    feat: Task 3, closure and loops

---
+++ Javascript_advanced/3-classrooms.js
@@ -0,0 +1,15 @@
+function createClassRoom (numbersOfStudents) {
+    function studentSeat(seat) {
+        return function() {
+            return seat;
+        }
+    }
+
+    let students = [];
+    for (let i = 1; i <= numbersOfStudents; i++) {
+        students.push(studentSeat(i));
+    }
+    return students;
+}
+
+const classRoom = createClassRoom(10);
