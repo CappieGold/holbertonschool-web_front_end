625c1026224fd15c5bcba939eba2ad6a710e73ce
CappieGold <144028326+CappieGold@users.noreply.github.com>
2025-04-11 12:44:15 +0000

    feat(task 10): Execution stack & timing execution

---
+++ Javascript_advanced/10-prime.js
@@ -0,0 +1,30 @@
+function countPrimeNumbers() {
+    let count = 0;
+    for (let i = 2; i <= 100; i++) {
+      let isPrime = true;
+      for (let j = 2; j <= Math.sqrt(i); j++) {
+        if (i % j === 0) {
+          isPrime = false;
+          break;
+        }
+      }
+      if (isPrime) {
+        count++;
+      }
+    }
+    return count;
+  }
+  
+  const iterations = 100;
+  const startTime = performance.now();
+  
+  let totalPrimes;
+  for (let k = 0; k < iterations; k++) {
+    totalPrimes = countPrimeNumbers();
+  }
+  
+  const endTime = performance.now();
+  const executionTime = endTime - startTime;
+  
+  console.log(`Execution time of calculating prime numbers 100 times was ${executionTime} milliseconds.`);
+  
